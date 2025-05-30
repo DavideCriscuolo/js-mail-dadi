d588935e0eb765db05c0aec7023ff2ba3bbbc5aa
DavideCriscuolo <davide.alcatel@gmail.com>
2025-05-30 18:01:14 +0200

    Aggiungi commenti esplicativi per la generazione dei numeri casuali e la determinazione del vincitore

--- js-dado/main.js
+++ js-dado/main.js
@@ -1,9 +1,13 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 for (let i = 0; i < 1; i++) {
+  //genera un numero random da 1 fino al 6 matendendo il valore intero con floor
   let numbRandomPersona = Math.floor(Math.random() * 6 + 1);
+  //genera un numero random da 1 fino al 6 matendendo il valore intero con floor
   let numbRandomPc = Math.floor(Math.random() * 6 + 1);
   console.log(numbRandomPersona, numbRandomPc);
+
+  //diamo la condizione per stabiliri chi sia il maggiore
   if (numbRandomPersona > numbRandomPc) {
     console.log("Vince la persona");
   } else if (numbRandomPersona < numbRandomPc) {
