0dbd1c00198340ac3d44d42c52fe69caf468144a
DavideCriscuolo <davide.alcatel@gmail.com>
2025-05-30 15:13:47 +0200

    Aggiungi file HTML e JavaScript per la gestione del gioco del dado

---
+++ js-dado/main.js
@@ -0,0 +1,14 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+for (let i = 0; i < 1; i++) {
+  let numbRandomPersona = Math.floor(Math.random() * 6);
+  let numbRandomPc = Math.floor(Math.random() * 6);
+  console.log(numbRandomPersona, numbRandomPc);
+  if (numbRandomPersona > numbRandomPc) {
+    console.log("Vince la persona");
+  } else if (numbRandomPersona < numbRandomPc) {
+    console.log("Vince il pc");
+  } else {
+    console.log("Pari");
+  }
+}
