e31f354a1f022342fd1a7cfb04e5257f10a192f6
DavideCriscuolo <davide.alcatel@gmail.com>
2025-05-30 15:14:57 +0200

    aggiunto +1 al metodo per escludere lo 0

--- js-dado/main.js
+++ js-dado/main.js
@@ -1,8 +1,8 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 for (let i = 0; i < 1; i++) {
-  let numbRandomPersona = Math.floor(Math.random() * 6);
-  let numbRandomPc = Math.floor(Math.random() * 6);
+  let numbRandomPersona = Math.floor(Math.random() * 6 + 1);
+  let numbRandomPc = Math.floor(Math.random() * 6 + 1);
   console.log(numbRandomPersona, numbRandomPc);
   if (numbRandomPersona > numbRandomPc) {
     console.log("Vince la persona");
